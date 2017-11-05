4189430a319ce0af0f84e0f518b90a21e72ebc27
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-05 22:51:28 +0000

    profiles

--- app/pages/speaker.js
+++ app/pages/speaker.js
@@ -3,10 +3,18 @@ import Vue from "vue";
 import { image, resize } from "../filters";
 import { get_data, extract_speaker } from "../data";
 
+var SOCIAL_INFO = {
+  'ABOUT.ME': 'https://about.me/',
+  'FACEBOOK': 'https://facebook.com/',
+  'GITHUB': 'https://github.com/',
+  'GPLUS': 'https://plus.google.com/+',
+  'LINKEDIN': 'https://www.linkedin.com/in/',
+  'TWITTER': 'https://twitter.com/',
+};
+
 var Speaker = Vue.component("speaker-page", {
   template: "#tpl-pages-speaker",
   props: ['id'],
-  filters: { image: image },
   data() {
     return {
       speaker: null,
@@ -18,18 +26,24 @@ var Speaker = Vue.component("speaker-page", {
   },
   watch: {'$route': 'init'},
   methods: {
+    image: image,
     resize: resize,
     init() {
       get_data()
         .then((results) => {
           this.speaker = extract_speaker(results.data, this.id);
           this.title = this.speaker.name;
-          console.log(this.speaker);
           this.$router.set_title(this.title);
         })
         .catch(error => {
           console.error(error);
         });
+    },
+    social_link(handle) {
+      return SOCIAL_INFO[handle.site] + handle.username;
+    },
+    social_icon(handle) {
+      return image(`img/social/${handle.site.toLowerCase()}.png`);
     }
   }
 });

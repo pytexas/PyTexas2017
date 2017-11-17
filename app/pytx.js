fd8813e7503b2233a8c260ed2f8c1363c26215b3
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-17 00:10:16 +0000

    safari 10 fixes

--- app/pytx.js
+++ app/pytx.js
@@ -144,3 +144,28 @@ function start_socket() {
     }
   };
 }
+
+function sayswho (){
+    var ua= navigator.userAgent, tem, 
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return M.join(' ');
+}
+
+if (sayswho() == 'Safari 10') {
+  var head  = document.getElementsByTagName('head')[0];
+  var link  = document.createElement('link');
+  link.rel  = 'stylesheet';
+  link.type = 'text/css';
+  link.href = URLS.static + 'less/safari-10-sucks.css';
+  head.appendChild(link);
+}
